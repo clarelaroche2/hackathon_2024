eafcb23eeb1e2a67e1cfe0c8fc6bbabe1c23e535
clare laroche <clare.laroche@saildrone.com>
2024-12-19 21:32:53 -0800

    made all cases lower case, fixed formatting error

--- js/mainGame.js
+++ js/mainGame.js
@@ -3,66 +3,66 @@ const gameData = {
         "text": "Congrats! You're going out on deployment... where are you going?",
         "image" : "smaller_images/snackies.png",
 "choices": {
-            "Key West": [2,["AIS","Genset","Ptz","Starlink"]],
-            "St Thomas": [2,["Adcp"]],
-            "San Diego": [2,["Ptz", "Genset", "Ais"]],
-            "Caymans": [2,["Starlink","Ptz","Adcp"]],
+            "Key West": [2,["ais","genset","ptz","starlink"]],
+            "St Thomas": [2,["adcp"]],
+            "San Diego": [2,["ptz", "genset", "ais"]],
+            "Caymans": [2,["starlink","ptz","adcp"]],
         }
     },
     "2": {
         "text": "What is the purpose of your mission??",
         "image" : "smaller_images/where_to.png",
 "choices": {
-            "Revolutionizing ocean science, of course!": [3,["Adcp"]],
-            "Military version of I spy": [3, ["Ptz", "Genset", "Ais", "Starlink"]],
-            "I'm on an exciting R&D mission, testing out some new technology we've put into the ocean for the first time": [3, ["Starlink", "Adcp", "Genset"]]
+            "Revolutionizing ocean science, of course!": [3,["adcp"]],
+            "Military version of I spy": [3, ["ptz", "genset", "ais", "starlink"]],
+            "I'm on an exciting R&D mission, testing out some new technology we've put into the ocean for the first time": [3, ["starlink", "adcp", "genset"]]
         }
     },
     "3": {
         "text": "After some great preparation and clearing TechOps and Pilot checkouts, you're ready to go! Deployment goes off without a hitch, but soon you find yourself in the middle of a crazy storm. The waves are heaving, and the wind is gusting at over 30 knots! What do you do?",
         "image" : "smaller_images/at_beach.png",
 "choices": {
-            "Fall into the water... the wave came out of nowhere!!": [4,["Adcp", "Ais"]],
-            "Hunker down! It makes the most sense to wait this one out": [4,["Genset", "Ptz", "Starlink"]],
-            "What an opportunity! I'm curious what data we could collect here...": [4,["Adcp", "Ptz"]],
+            "Fall into the water... the wave came out of nowhere!!": [4,["adcp", "ais"]],
+            "Hunker down! It makes the most sense to wait this one out": [4,["genset", "ptz", "starlink"]],
+            "What an opportunity! I'm curious what data we could collect here...": [4,["adcp", "ptz"]],
         }
     },
     "4": {
         "text": "OVERRRRBOAAARRRRDDDD! As you sink into the water hit ground much sooner than expected. You have discovered a sea mount",
         "image" : "smaller_images/at_beach.png",
 "choices": {
-            "Awesome!! I gotta document EVERYTHING. I hope I see a mermaid": [6,["Adcp", "Ptz"]],
-            "Who cares?? It's still the water. Start looking for ways home" : [6,["Genset", "Starlink", "Ais"]],
-            "Gollie Geeee! I wonder if there are boats down here...": [6,["Ais", "Ptz"]],
+            "Awesome!! I gotta document EVERYTHING. I hope I see a mermaid": [6,["adcp", "ptz"]],
+            "Who cares?? It's still the water. Start looking for ways home" : [6,["genset", "starlink", "ais"]],
+            "Gollie Geeee! I wonder if there are boats down here...": [6,["ai", "ptz"]],
         }
     },
     "5": {
         "text": "The storm is getting really rough. It's been days since you've seen the sun",
         "image" : "smaller_images/at_beach.png",
 "choices": {
-            "Pray to every god until one answers": [6,["Adcp", "Ais", "Genset"]],
-            "You've seen Treasure Planet, Moana 2, and Captain and Commander. You can absolutely navigate your way through this. Press on": [6,["Ais", "Ptz", "Starlink"]],
-            "It's time to be realistic. Start planning who's power you are going to pull first.": [6,["Genset", "Ptz", "Ais"]],
+            "Pray to every god until one answers": [6,["adcp", "ai", "genset"]],
+            "You've seen Treasure Planet, Moana 2, and Captain and Commander. You can absolutely navigate your way through this. Press on": [6,["ais", "ptz", "starlink"]],
+            "It's time to be realistic. Start planning who's power you are going to pull first.": [6,["genset", "ptz", "ais"]],
         }
     },
     "6": {
         "text": "You are finally on mission, yay! As you sail off you notice bird swoops down and sits on you. What do you do next?",
         "image" : "smaller_images/at_beach.png",
 "choices": {
-            "SCREAM and tell it to get off": [0,["Genset","Ais"]],
-            "Say hi! Birds are cool": [0,["Adcp", "Ptz"]],
-            "Try to figure out if it is really a bird...": [0,["Ais, Ptz"]],
-            "Die": [0,["Starlink"]],
+            "SCREAM and tell it to get off": [0,["genset","ais"]],
+            "Say hi! Birds are cool": [0,["adcp", "ptz"]],
+            "Try to figure out if it is really a bird...": [0,["ais", "ptz"]],
+            "Die": [0,["starlink"]],
         }
     },
 };
 
 const personalities = {
-    "Ptz": 0, // Nosey bitch
-    "Starlink": 0, // Elon Sympathizer
-    "Genset": 0, // Gym bro
-    "Ais": 0, // Narc
-    "Adcp": 0, // Ocean enjoyer (flamboyantly)
+    "ptz": 0, // Nosey bitch
+    "starlink": 0, // Elon Sympathizer
+    "genset": 0, // Gym bro
+    "ais": 0, // Narc
+    "adcp": 0, // Ocean enjoyer (flamboyantly)
  }; 
 
 let currentState = 1;
